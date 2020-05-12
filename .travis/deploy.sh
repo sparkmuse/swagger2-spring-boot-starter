48508ca3e421cd9405802e671fc86c21da6430bb
Sparkmuse <sparkmuse.tech@gmail.com>
2020-05-12 08:52:02 +0200

    New yaml format

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,4 +3,4 @@
 mvn versions:set -DnewVersion=1.1.$(git rev-list HEAD --count)
 
 # Deploy
-mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U -Prelease,ossrh -X
+mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U -Prelease,ossrh
